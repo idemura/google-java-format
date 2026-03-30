9ac03c0f116267800d843d20b6b5863d0db62731
Igor Demura <igor.demura@gmail.com>
2026-03-30 20:03:29 +0300

    TTech: Update plugin

--- idea_plugin/src/main/java/com/google/googlejavaformat/intellij/UiFormatterStyle.java
+++ idea_plugin/src/main/java/com/google/googlejavaformat/intellij/UiFormatterStyle.java
@@ -24,7 +24,8 @@ import java.util.Objects;
 /** Configuration options for the formatting style. */
 enum UiFormatterStyle {
   GOOGLE("Default Google Java style", Style.GOOGLE),
-  AOSP("Android Open Source Project (AOSP) style", Style.AOSP);
+  AOSP("Android Open Source Project (AOSP) style", Style.AOSP),
+  TTECH("TTech style", Style.TTECH);
 
   private final String description;
   private final JavaFormatterOptions.Style style;
